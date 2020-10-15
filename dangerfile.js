27431eb530bfdad4631f0c74bd27ea8c0b1e4806
zarifergashev <zarifergashev4@gmail.com>
2020-10-15 08:52:05 +0500

    changed dangerfile.js file

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,4 @@
 import {message, danger} from "danger"
 
-const modifiedMD = danger.git.modified_files.join("- ")
+const modifiedMD = danger.git.modified_files.join("MainActivity")
 message("Changed Files in this PR: \n - " + modifiedMD)
